6579d3d0495aa4bfd97c2ad16d482adf43b664f3
Yevhen Shulha <yevhen.shulha@p2h.local>
2020-09-30 16:48:49 +0300

    section 2 is ready

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,3 +1,16 @@
+// burger-button
 $('.header__burger').on('click', function(){
 	$('.header__nav').toggleClass('active');
+});
+
+// fancybox
+$('[data-fancybox="gallery"]').fancybox({
+	buttons : [
+		'slideShow',
+		'share',
+		'zoom',
+		'fullScreen',
+		'close'
+	],
+	protect: true
 });
