bc6ccb01d08c598284b1cd4033c2ff613737f2eb
Gekagmeka <shulgagggeka@gmail.com>
2021-01-30 16:57:07 +0200

    smooth scroll and decrease size of the images

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -3,6 +3,10 @@ $('.header__burger').on('click', function(){
 	$('.header__nav').toggleClass('active');
 	$('body').toggleClass('locked');
 });
+$('.header__link').on('click', function(){
+	$('.header__nav').removeClass('active');
+	$('body').removeClass('locked');
+});
 
 //leave__button
 $('[data-toggle="open-modal"]').on('click', function(){
@@ -15,6 +19,30 @@ $('[data-toggle="close-modal"]').on('click', function(){
 	$('body').removeClass('locked');
 	//$('.contact__right').closest('body').toggleClass('locked'); //to delete scrool during opened form popup
 });
+
+
+
+//smooth scrolling to the anchor
+//this.hash -> подтягивает именно тот id, по ссылке которого был клик
+// $('.header__link').on('click', function(e){
+// 	$('html, body').stop().animate({
+// 		scrollTop: $(this.hash).offset().top }, 1000
+// 		);
+// 	e.preventDefault();
+//  });
+
+//можно использовать этот код
+//тут неважно если ли ссылки с определенными классами
+//тут отслеживаем клик по ссылкам - у которых есть # в href
+$('body').on('click', '[href*="#"]', function(e){
+	//var fixed_offset = 100; - это смещение (высота верхнего блока)
+	//$('html, body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+	$('html, body').stop().animate({ scrollTop: $(this.hash).offset().top }, 1000);
+	e.preventDefault();
+ });
+
+
+
 
 // fancybox
 $('[data-fancybox="gallery"]').fancybox({
